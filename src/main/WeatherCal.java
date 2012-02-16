146af3ab0c40a86bf7ce87c5ce63557eb063acf3
Michael Caputo <therubikscubekid@gmail.com>
2012-02-16 18:01:58 -0500

    First commit

---
+++ src/main/WeatherCal.java
@@ -0,0 +1,31 @@
+package main;
+//************************************************************
+//	This Project is a simple alarm clock that will change
+//    the song played based on the weather outside. The
+//    point of it is really for me to play around with
+//    learning git better, as well as to experiment
+//    with the Google Weather API.
+//************************************************************
+
+/** $Author: Michael Caputo */
+
+
+// specify the package
+
+// system imports
+
+// project includes
+
+//==============================================================
+
+public class WeatherCal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
